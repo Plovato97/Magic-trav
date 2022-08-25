6deaec09aab3b80133a1bf59f5bd36f14f54d144
paul lovato <lovato.paul97@yahoo.com>
2022-08-25 03:08:34 -0400

    All api's are working and logged in from the initial search parameters

--- assets/js/adventure.js
+++ assets/js/adventure.js
@@ -1,48 +1,76 @@
 var paulApiKey = "975536fb3351415c84e19797480ca9b3"
+var paulGoogleKey = "cf8b1e356c94fd5a088afa5884193906daf90a725ab4763d792e35a05f4a8ef9"
 
 var cityGet = document.querySelector("#user-form")
 var inputEl = document.querySelector("#cityInput")
 var dateEl = document.querySelector("#dateInput")
+var statesEl = document.querySelector("#statesSelect")
 
 
-// var requestOptions = {
-//     method: 'GET',
-//   };
-  
-// //   fetch("https://api.geoapify.com/v1/geocode/search?text=38%20Upper%20Montagu%20Street%2C%20Westminster%20W1H%201LJ%2C%20United%20Kingdom&apiKey=" + paulApiKey + "", requestOptions)
-// //     .then(response => response.json())
-// //     .then(result => console.log(result))
-// //     .catch(error => console.log('error', error));
+var routePlan = function(long, lat, long2, lat2) {
+var routeApi = "https://api.geoapify.com/v1/routing?waypoints=" + long + "," + lat + "|" + long2 + "," + lat2 + "&mode=drive&apiKey=" + paulApiKey + "";
+fetch(routeApi)
+    .then(function (res) {
+        return res.json();
+    })
+    .then(function (data) {
+        console.log(data);
+        getCord(data)
+    })
+};
 
 
-var getCityCord = function(citySearch) {
-    var cordApiUrl = "https://api.geoapify.com/v1/geocode/search?text=" + citySearch + "&apiKey=" + paulApiKey + "";
+var eventsFun = function(citySearch, state) {
+    var eventsApi = "https://cors-anywhere.herokuapp.com/https://serpapi.com/search.json?engine=google_events&q=Events+in+" + citySearch + "+" + state + "&api_key=cf8b1e356c94fd5a088afa5884193906daf90a725ab4763d792e35a05f4a8ef9";
+    fetch(eventsApi)
+        .then(function (res) {
+            return res.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            // getCord(data)
+        })
+    };
+    
+
+var searchCity = function(citySearch, state) {
+    var cordApiUrl = "https://api.geoapify.com/v1/geocode/search?text=" + citySearch + "%20" + state + "&apiKey=" + paulApiKey + "";
     fetch(cordApiUrl)
         .then(function (res) {
             return res.json();
         })
         .then(function (data) {
             console.log(data);
+            getCord(data)
         })
     };
 
+var getCord = function(data) {
+    var cityLattidue = data.features[0].properties.lat;
+    var cityLongitude = data.features[0].properties.lon;
+    console.log(cityLattidue);
+    console.log(cityLongitude);
+}
     var formSubmit = function(event) {
         event.preventDefault();
     
         // get value from input element
         var cityName = inputEl.value.trim();
-        var cityDate = dateEl.value
+        var cityDate = dateEl.value;
+        var statesSelector = statesEl.value;
     
-        if (cityName && cityDate) {
-            getCityCord(cityName);
+        if (cityName && cityDate && statesSelector) {
+            eventsFun(cityName, statesSelector);
+            searchCity(cityName, statesSelector);
             inputEl.value = "";
             dateEl.value = "";
+            statesEl.value = "";
         } else if (cityName) {
             alert("Please Select a date")
         } else if (cityDate) {
             alert ("please select a City")
         } else {
-            alert ("Please submit a correct City and Date")
+            alert ("Please submit a correct City, State and Date")
         }
         console.log(event);
     };
