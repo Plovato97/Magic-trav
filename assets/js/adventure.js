261400db890cc5998f2e1512387de931febb62b4
paul lovato <lovato.paul97@yahoo.com>
2022-08-27 20:15:51 -0400

    randomization function completed. Added fortune teller API

--- assets/js/adventure.js
+++ assets/js/adventure.js
@@ -6,6 +6,7 @@ var inputEl = document.querySelector("#cityInput")
 var dateEl = document.querySelector("#dateInput")
 var statesEl = document.querySelector("#statesSelect")
 var divTest = document.querySelector("#testLinks")
+var fortuneEl = document.querySelector("#fortuneTest")
 
 // uses the submit form eleement to gather the date/city/state paremeters to be used on other functions
 var formSubmit = function(event) {
@@ -41,43 +42,81 @@ var eventsFun = function(citySearch, state, dates) {
         })
         .then(function (data) {
             console.log(data);
-            shuffleArray(data);
+            eventsLoop(data);
         })
     };
 
-const shuffleArray = function (data) {
-    var arrayShuffle = data.events_results;
-        for (let i = arrayShuffle.length - 1; i > 0; i--) {
-          const j = Math.floor(Math.random() * (i + 1));
-          const temp = arrayShuffle[i];
-          arrayShuffle[i] = arrayShuffle[j];
-          arrayShuffle[j] = temp;
-
-        //   console.log(arrayShuffle[i])
-        eventsLoop(arrayShuffle[i]);
-
-        // console.log(arrayShuffle[j].event_location_map.link)
+function shuffle(array) {
+        let currentIndex = array.length,  randomIndex;
+      
+        // While there remain elements to shuffle.
+        while (currentIndex != 0) {
+      
+          // Pick a remaining element.
+          randomIndex = Math.floor(Math.random() * currentIndex);
+          currentIndex--;
+      
+          // And swap it with the current element.
+          [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
         }
-};
+      
+        return array;
+      }
 
 var eventsLoop = function(data) {
 
-    console.log(data)
+var eventRandom = shuffle(data.events_results);
 
-for (var i = 0; i < data.length; i++) {
+console.log(eventRandom)
 
     var resultsSpan = document.createElement("div");
-    resultsSpan.textContent = data[i].link;
+    resultsSpan.textContent = eventRandom[1].event_location_map.link;
 
     var titleSpan = document.createElement("div");
-    titleSpan.textContent = data[i].title;
+    titleSpan.textContent = eventRandom[1].title;
 
     var descriptionSpan = document.createElement("div");
-    descriptionSpan.textContent = data[i].description;
+    descriptionSpan.textContent = eventRandom[1].description;
 
-    divTest.append(titleSpan);
-    divTest.append(descriptionSpan);
-    divTest.append(resultsSpan);
-}};
+    divTest.appendChild(titleSpan);
+    divTest.appendChild(descriptionSpan);
+    divTest.appendChild(resultsSpan);
+
+};
+
+    var fortuneFun = function() {
+
+        var fortuneApi = "https://digital-fortune-cookies-api.herokuapp.com/fortune?amount=7";
+        fetch(fortuneApi)
+            .then(function (res) {
+                return res.json();
+            })
+            .then(function (data) {
+                console.log(data);
+                fortuneLoop(data);
+            })
+        };
+
+var fortuneLoop = function(data) {
+    var fortune = data.cookie.fortune;
+    var luckNumber = data.cookie.luckyNumbers;
+
+var fortuneTeller = document.createElement("span")
+fortuneTeller.textContent = fortune;
+fortuneEl.append(fortuneTeller);
+
+    for (var i = 0; i < luckNumber.length; i++) {
+        console.log(luckNumber[i]);
+
+        var fortuneDescription = document.createElement("div")
+        fortuneDescription.textContent = luckNumber[i];
+        fortuneEl.append(fortuneDescription);
+}
+
+};
 
 cityGet.addEventListener("submit", formSubmit);
+
+
+// adventureSubmit.addEventListener("submit", fortuneFun)
