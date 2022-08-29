99e571845b6964510e03ea5b989707907097ef45
Myeisha Swain <zse0296@gmail.com>
2022-08-28 23:29:14 -0400

    styling card

--- assets/js/adventure.js
+++ assets/js/adventure.js
@@ -82,10 +82,11 @@ var eventsLoop = function(data) {
 
 var eventRandom = shuffle(data.events_results);
 
+
+/* Card ONE*/
     var resultsBtn= document.createElement("a");
     resultsBtn.setAttribute("target", "_blank")
     resultsBtn.textContent = "Begin your Journey"
-    resultsBtn.classList.add("button",)
     resultsBtn.href = eventRandom[1].event_location_map.link;
 
     var titleSpan = document.createElement("div");
@@ -93,11 +94,13 @@ var eventRandom = shuffle(data.events_results);
     titleSpan.textContent = eventRandom[1].title;
 
     var descriptionSpan = document.createElement("div");
-    descriptionSpan.classList.add("card__body")
+    descriptionSpan.classList.add("card__body");
     descriptionSpan.textContent = eventRandom[1].description;
 
+
+    /* Code to add image to card 1*/
     var imageHolder = document.createElement("img");
-    imageHolder.src = "assets/images/placeholder.jpeg";
+    imageHolder.src = "assets/images/placeholder.png";
     var src = document.getElementById("imgE1");
     src.appendChild(imageHolder);
 
@@ -105,77 +108,130 @@ var eventRandom = shuffle(data.events_results);
     divTest1.appendChild(descriptionSpan);
     divTest1.appendChild(resultsBtn);
 
+
+/* CARD 2 */
     var resultsBtn2= document.createElement("a");
     resultsBtn2.setAttribute("target", "_blank")
     resultsBtn2.textContent = "Begin your Journey"
     resultsBtn2.href = eventRandom[2].event_location_map.link;
 
     var titleSpan2 = document.createElement("div");
+    titleSpan2.classList.add("card__mobile-title")
     titleSpan2.textContent = eventRandom[2].title;
 
     var descriptionSpan2 = document.createElement("div");
+    descriptionSpan2.classList.add("card__body");
     descriptionSpan2.textContent = eventRandom[2].description;
+
+     /* Code to add image to card 2*/
+     var imageHolder = document.createElement("img");
+     imageHolder.src = "assets/images/placeholder.png";
+     var src = document.getElementById("imgE2");
+     src.appendChild(imageHolder);
 
     divTest2.appendChild(titleSpan2);
     divTest2.appendChild(descriptionSpan2);
     divTest2.appendChild(resultsBtn2);
 
-   
+    
+
+
+
+/* CARD 3 */
     var resultsBtn3= document.createElement("a");
     resultsBtn3.setAttribute("target", "_blank")
     resultsBtn3.textContent = "Begin your Journey"
     resultsBtn3.href = eventRandom[3].event_location_map.link;
 
     var titleSpan3 = document.createElement("div");
+    titleSpan3.classList.add("card__mobile-title")
     titleSpan3.textContent = eventRandom[3].title;
 
     var descriptionSpan3 = document.createElement("div");
+    descriptionSpan3.classList.add("card__body");
     descriptionSpan3.textContent = eventRandom[3].description;
+
+     /* Code to add image to card 2*/
+     var imageHolder = document.createElement("img");
+     imageHolder.src = "assets/images/placeholder.png";
+     var src = document.getElementById("imgE3");
+     src.appendChild(imageHolder);
+    
 
     divTest3.appendChild(titleSpan3);
     divTest3.appendChild(descriptionSpan3);
     divTest3.appendChild(resultsBtn3);
 
+
+/*CARD 4 */
     var resultsBtn4= document.createElement("a");
     resultsBtn4.setAttribute("target", "_blank")
     resultsBtn4.textContent = "Begin your Journey"
     resultsBtn4.href = eventRandom[4].event_location_map.link;
 
     var titleSpan4 = document.createElement("div");
+    titleSpan4.classList.add("card__mobile-title")
     titleSpan4.textContent = eventRandom[4].title;
 
     var descriptionSpan4 = document.createElement("div");
+    descriptionSpan4.classList.add("card__body");
     descriptionSpan4.textContent = eventRandom[4].description;
+
+     /* Code to add image to card 2*/
+     var imageHolder = document.createElement("img");
+     imageHolder.src = "assets/images/placeholder.png";
+     var src = document.getElementById("imgE4");
+     src.appendChild(imageHolder);
 
     divTest4.appendChild(titleSpan4);
     divTest4.appendChild(descriptionSpan4);
     divTest4.appendChild(resultsBtn4);
 
+
+/* CARD 5 */
     var resultsBtn5= document.createElement("a");
     resultsBtn5.setAttribute("target", "_blank")
     resultsBtn5.textContent = "Begin your Journey"
     resultsBtn5.href = eventRandom[5].event_location_map.link;
 
     var titleSpan5 = document.createElement("div");
+    titleSpan5.classList.add("card__mobile-title")
     titleSpan5.textContent = eventRandom[5].title;
 
     var descriptionSpan5 = document.createElement("div");
+    descriptionSpan5.classList.add("card__body");
     descriptionSpan5.textContent = eventRandom[5].description;
+
+     /* Code to add image to card 2*/
+     var imageHolder = document.createElement("img");
+     imageHolder.src = "assets/images/placeholder.png";
+     var src = document.getElementById("imgE5");
+     src.appendChild(imageHolder);
 
     divTest5.appendChild(titleSpan5);
     divTest5.appendChild(descriptionSpan5);
     divTest5.appendChild(resultsBtn5);
 
+
+/* CARD 6 */
     var resultsBtn6= document.createElement("a");
     resultsBtn6.setAttribute("target", "_blank")
     resultsBtn6.textContent = "Begin your Journey"
     resultsBtn6.href = eventRandom[6].event_location_map.link;
 
     var titleSpan6 = document.createElement("div");
+    titleSpan6.classList.add("card__mobile-title")
     titleSpan6.textContent = eventRandom[6].title;
 
     var descriptionSpan6 = document.createElement("div");
+    descriptionSpan6.classList.add("card__body");
     descriptionSpan6.textContent = eventRandom[6].description;
+
+     /* Code to add image to card 2*/
+     var imageHolder = document.createElement("img");
+     imageHolder.src = "assets/images/placeholder.png";
+     var src = document.getElementById("imgE6");
+     src.appendChild(imageHolder);
 
     divTest6.appendChild(titleSpan6);
     divTest6.appendChild(descriptionSpan6);
